beab699f662caa8b0bbbe512062d232fc5192d57
MM7mDM <111407171+MM7mDM@users.noreply.github.com>
2022-11-15 00:12:35 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="اهلا بك في!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          m7plus <code>pages/index.js</code>
         </p>
       </main>
 
